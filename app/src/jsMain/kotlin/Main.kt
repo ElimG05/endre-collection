6e2e5c13fae6ab7199db8483afb222661e12c371
ElimG <bruker@Mac.lan>
2026-09-08 21:12:57 +0200

    feat(booklet): add swipe navigation between pages

--- app/src/jsMain/kotlin/Main.kt
+++ app/src/jsMain/kotlin/Main.kt
@@ -1,21 +1,31 @@
 import kotlinx.browser.document
 import kotlinx.browser.window
 import org.w3c.dom.events.KeyboardEvent
+import kotlin.math.abs
 
-var currentIndex = 0;
-var touchStartX = 0.0;
 
-fun main() {
-    showPage()
-    window.addEventListener("keydown", {e -> 
-        when ((e as KeyboardEvent).key) {
-            "ArrowRight" -> next()
-            "ArrowLeft" -> prev()
-        
-            else -> {}
+var currentIndex = 0
+var touchStartX = 0.0 
+
+fun swipePage() {
+    window.addEventListener("touchstart", {e -> 
+        val touchEvent = e.asDynamic().changedTouches[0]
+        //the position where the swipe starts
+        touchStartX = touchEvent.clientX as Double
+    })
+    window.addEventListener("touchend", {e ->
+        val touchEvent = e.asDynamic().changedTouches[0]
+        //check where the swipe ends
+        val touchEndX = touchEvent.clientX as Double
+        val diffX = touchEndX - touchStartX
+        if (abs(diffX) > 50) {
+            if (diffX > 0)
+                prev() //negative = swipe left
+                else 
+                    next() //positive = swipe right,
         }
     })
-}   
+}
 
 fun showPage() {
     val page = pages[currentIndex]
@@ -26,3 +36,16 @@ fun showPage() {
         </div>
         """
 }
+
+fun main() {
+    showPage()
+    swipePage()
+    window.addEventListener("keydown", {e -> 
+        when ((e as KeyboardEvent).key) {
+            "ArrowRight" -> next()
+            "ArrowLeft" -> prev()
+        
+            else -> {}
+        }
+    })
+}   
